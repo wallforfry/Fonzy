e4b33c7cccc930d5b9d4259cd4dcb05d56bba6e8
Wallerand Delevacq <wallforfry@gmail.com>
2019-02-25 19:33:20 +0100

    Add models

--- sources/Stocks/admin.py
+++ sources/Stocks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from Stocks.models import Categorie, Element
+
+admin.site.register(Categorie)
+admin.site.register(Element)
--- sources/Stocks/models.py
+++ sources/Stocks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Categorie(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Catégorie")
+    description = models.TextField(default="", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Element(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Nom")
+    description = models.TextField(default="", blank=True)
+    count = models.IntegerField(verbose_name="Quantité", default=1)
+    categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, verbose_name="Catégorie", null=True, blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="Prix", default=0)
+    caution = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="Caution", default=0)
+    image = models.ImageField(verbose_name="Image", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
