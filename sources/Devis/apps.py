6aa5cb98cc6ea94de3cb63279ac6b043258f4c51
Wallerand Delevacq <wallforfry@gmail.com>
2019-03-11 13:46:05 +0100

    Add Devis App

---
+++ sources/Devis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DevisConfig(AppConfig):
+    name = 'Devis'
--- sources/Fonzy/settings.py
+++ sources/Fonzy/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_gravatar',
     'Stocks',
-    'Backend'
+    'Backend',
+    'Devis'
 ]
 
 MIDDLEWARE = [
